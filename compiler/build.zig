2eb9e36b2c895aa6731691f8da4b289d3aa0e341
mtctx <me@mtctx.dev>
2025-08-31 19:46:10 +0200

    Cleaned up build.zig & build.zig.zon

--- compiler/build.zig
+++ compiler/build.zig
@@ -6,16 +6,32 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "parser",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     b.installArtifact(exe);
 
+    const run_step = b.step("run", "Run the app");
+
     const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
     run_cmd.step.dependOn(b.getInstallStep());
 
-    const run_step = b.step("run", "Run the parser");
-    run_step.dependOn(&run_cmd.step);
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_exe_tests.step);
 }
